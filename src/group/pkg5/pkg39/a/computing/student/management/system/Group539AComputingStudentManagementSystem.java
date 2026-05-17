eeba9f19a03c64d9f5062eb75a0f88826fe80955
prateeksatyal <prateeksatyal51@gmail.com>
2026-05-17 10:51:53 +0545

    gitinore add

--- src/group/pkg5/pkg39/a/computing/student/management/system/Group539AComputingStudentManagementSystem.java
+++ src/group/pkg5/pkg39/a/computing/student/management/system/Group539AComputingStudentManagementSystem.java
@@ -15,6 +15,7 @@ public class Group539AComputingStudentManagementSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
