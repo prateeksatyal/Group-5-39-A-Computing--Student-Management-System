a2f583e22f57db327d5e3218c34f84b4d56442a3
Insha Dangol <samiindra2063@gmail.com>
2026-05-19 12:00:00 +0545

    Connected Signup with Database

--- src/model/UserData.java
+++ src/model/UserData.java
@@ -5,7 +5,7 @@
 package model;
 
 public class UserData {
-    private int user_id;
+    private int userId;
     private String username;
     private String password;
     private String email;
@@ -18,13 +18,13 @@ public class UserData {
         this.role = role;
     }
     
-    public int getId(){ return user_id; }
+    public int getUserId(){ return userId; }
     public String getUserName(){ return username; }
     public String getPassword(){ return password; }
     public String getEmail(){ return email; }
     public String getRole(){ return role; }
     
-    public void setUserId(int user_id){ this.user_id = user_id; }
+    public void setUserId(int userId){ this.userId = userId; }
     public void setUserName(String username){ this.username = username; }
     public void setPassword(String password){ this.password = password; }
     public void setEmail(String email){ this.email = email; }
